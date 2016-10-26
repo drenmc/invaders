b67f2be4382572cf5dfb7bc46d89b73298190959
Charlie Huguenard <charlie.huguenard@gmail.com>
2016-10-26 13:25:39 -0700

    added music system, wired up some stuff in the attract screen

---
+++ Assets/ModScripts/PlayRandomSound.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayRandomSound : MonoBehaviour 
+{
+	[SerializeField] private AudioClip[] _clips;
+
+	private AudioSource _audioSource;
+
+	public void Play()
+	{
+		var rand = Random.Range(0, _clips.Length);
+		_audioSource.clip = _clips[rand];
+		_audioSource.Play();
+	}
+
+	private void Awake()
+	{
+		_audioSource = GetComponent<AudioSource>();
+
+		if (_audioSource.playOnAwake)
+		{
+			Play();
+		}
+	}
+}
